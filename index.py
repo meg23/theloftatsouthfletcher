8b0757a442f47bcc8d53cfa4b8f0884445bcd78a
meg23 <m.gelman08@gmail.com>
2015-09-19 22:13:45 -0400

    so many.changes.

--- index.py
+++ index.py
@@ -17,25 +17,15 @@ class Index(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, ""))
 
-class Contact(webapp2.RequestHandler):
-
-    def get(self):
-        template_values = {
-            'header_title': "Contact",
-            'header_body': "Learn about Estee and what she loves to do"
-        }
-        path = os.path.join(os.path.dirname(__file__), 'contact.html')
-        self.response.out.write(template.render(path, template_values))
-
     def post(self):
         contact_name = self.request.get('name')
         contact_email = self.request.get('email')
         contact_subject = self.request.get('subject')
         contact_message = self.request.get('message') + "\n Signed %s, %s" % (contact_name, contact_email)
 
-        message = mail.EmailMessage(sender="estee@esteegelman.appspotmail.com",
+        message = mail.EmailMessage(sender="info@theloft-1034.appspotmail.com",
                             subject=contact_subject)
-        message.to = "estee@esteegelman.com"
+        message.to = "info@5211southfletcher.com"
         message.body = contact_message
         message.send()
 
@@ -43,8 +33,8 @@ class Contact(webapp2.RequestHandler):
             'header_title': "Thanks for reaching out %s!" % (contact_name.capitalize()),
             'header_body': "She will get back to you soon, I promise."
         }
-        path = os.path.join(os.path.dirname(__file__), 'contact.html')
-        self.response.out.write(template.render(path, template_values))
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, ""))
 
 class Meet(webapp2.RequestHandler):
 
@@ -52,6 +42,6 @@ class Meet(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'meet.html')
         self.response.out.write(template.render(path, ""))
 
-app = webapp2.WSGIApplication([('/', Index ), ( '/contact', Contact), ('/meet', Meet)], debug=True)
+app = webapp2.WSGIApplication([('/', Index )], debug=True)
 
 
